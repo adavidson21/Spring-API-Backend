28bafb4a766e8f460b8fd70fa770f1e94fb7bcfb
adavidson21 <alexis.davidson221@gmail.com>
2024-09-15 23:13:33 -0400

    Add initial react setup with nav bar & cards for UI

---
+++ jokeApi/src/main/react/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+React.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        Hello World!
+    </React.StrictMode>
+);
